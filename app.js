cc46ea8589f5a6c402d19dcb23ff1e5befab7391
SandeshOnRails <basnetsandesh450@gmail.com>
2018-07-07 21:59:07 -0700

    search optimization algorithm for search terms

--- app.js
+++ app.js
@@ -40,12 +40,17 @@ app.get('/search', function(req, res){
 app.post('/search/results', function(req, res){
 
 
-	  var results = {
+	  var actualResult = ''
 
-	  	  searchKey: req.body.searchKey
-	  }
+
+	  var searchTerm = checkForKey(req.body.searchKey)
+       
+
+	  if(!searchTerm) searchTerm = req.body.searchKey
+	  
      
      con.getConnection(function(err, connection) {
+
   if(err){
   	console.log("error connecting: " + err)
   }
@@ -53,14 +58,39 @@ app.post('/search/results', function(req, res){
   	console.log('connection successful')
   }
 
-  connection.query("SELECT * FROM photos WHERE description LIKE '%" + req.body.searchKey + "%'", function(err, result, fields){
+  connection.query("SELECT * FROM photos WHERE description LIKE '%" + searchTerm + "%'", function(err, result, fields){
 
-      if(err){
-        console.log(err)
+      if(err) {
+      	console.log("SQL ERROR AT : " + err)
       }
         else { 
+                  if(result.length > 0) {
+        	var showingResult = result[0].description
+        	var alldescription = showingResult.split(',')
+
+        	for(var i=0; i < alldescription.length; i++) {
+
+        		   if(alldescription[i].match(req.body.searchKey)) {
+
+        		   	  actualResult = alldescription[i]
+
+        		   }
+        	}
+        
+
+              console.log(req.body.searchKey)
+              console.log(searchTerm)
+        	
            
-               res.render('search-results', {result: result})
+               res.render('search-results', {result: result, searchKey:req.body.searchKey, actualResult: searchTerm})
+           }
+
+           else {
+
+          res.render('search-results', {result: result})
+
+
+           }
 
 
         }
@@ -73,6 +103,28 @@ app.post('/search/results', function(req, res){
 })
 
 
+const checkForKey = (searchKey)=> {
+  
+  var database = ['parrot', 'bird', 'dog', 'kingfisher', 'huskey', 'cute']
+
+
+             for(var i =0; i < database.length; i++) {
+
+             	   if(database[i].length > searchKey.length) {
+                         
+                         if(database[i].match(searchKey)) return database[i]
+             	   }
+
+             	else {
+                         
+                         if(searchKey.match(database[i])) return database[i]
+
+             	}
+             }
+   return ''
+
+                
+}
 
 
 server.listen(process.env.PORT || 3000, ()=> console.log("Server Running"));
