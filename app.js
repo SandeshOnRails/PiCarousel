ea3fbce228a357697f3fb454a0f28f3e43df3978
SandeshOnRails <basnetsandesh450@gmail.com>
2018-07-10 22:04:25 -0700

    middleware for upload to check file format

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ const io = require('socket.io')(server);
 
 const fileUpload = require('express-fileupload');
 app.use(fileUpload());
+app.use('/imgupload', function(req, res, next){
+  next()
+})
 
 app.set('view engine', 'ejs');
 
@@ -36,20 +39,23 @@ require('./routes/about.js')(express,app);
 
       app.get('/imgupload', function(req, res){
 
-         res.render('imgupload')
+         res.render('imgupload', {error:false})
       })
+
+
+      //post search results
 
       app.post('/search/results', function(req, res){
 
 
       	  var actualResult = ''
-
+            
+      // function to see if the searchKey matches the database descriptions
 
           checkForKey(req.body.searchKey, function(result){
 
               var searchTerm = result
-              console.log("Result: " + result)
-
+              
               if(!searchTerm) searchTerm = req.body.searchKey
      	  
               search(searchTerm, con, function(result){
@@ -81,13 +87,13 @@ require('./routes/about.js')(express,app);
       })
 
 
-      app.post('/upload', function(req, res) {
+      app.post('/upload', isFileValid, function(req, res) {
         if (!req.files)
           return res.status(400).send('No files were uploaded.');
        
         // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
         let sampleFile = req.files.sampleFile;
-        console.log(req.files.sampleFile.name);
+        console.log(typeof req.files.sampleFile.mimetype);
        
         // Use the mv() method to place the file somewhere on your server
         sampleFile.mv('assets/'+req.files.sampleFile.name, function(err) {
@@ -97,5 +103,20 @@ require('./routes/about.js')(express,app);
           res.send('File uploaded!');
         });
       });
+
+
+//middleware to check if the file is valid jpeg or png format
+
+function isFileValid(req, res, next) {
+
+  let error = 'whatever';
+
+  let type = req.files.sampleFile.mimetype.toString();
+  
+
+    if(type === 'image/jpeg' || type === 'image/png') return next()
+      res.render('imgupload', {error:error})
+}
+
 
 app.listen(process.env.PORT || 3000, ()=> console.log("Server Running"));
