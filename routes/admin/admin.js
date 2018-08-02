d807aa4ca36252e729de406426777efc746f60d1
altindag <caltindag@mail.sfsu.edu>
2018-08-02 07:05:26 -0700

    admin page limitation added for member and admin

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -8,13 +8,13 @@ module.exports = function (app,crypto) {
 	 	else{
 
 	 	console.log("password : "+crypto.decipher('secretKey','1b6bd58bde5f3729b55e3b7cdfadea3b' ))
-	 	console.log("password admin: "+crypto.cipher('secretKey', 'admin', 'aes256'));
-	 	console.log("password : admin"+crypto.decipher('secretKey', crypto.cipher('secretKey', 'admin', 'aes256')));
+	 	console.log("password admin: "+crypto.cipher('secretKey', 'member', 'aes256'));
+	 	console.log("password : admin"+crypto.decipher('secretKey', crypto.cipher('secretKey', 'member', 'aes256')));
 	 	console.log("when browse /admin page user name :"+ req.session.user);
 	 	console.log("when browse /admin page user id :"+ req.session.user_id);
 	 	console.log("when browse /admin page user account type :"+ req.session.user_accounttype);
 
-	 	 res.render('admin/viewAdmin')
+	 	 res.render('admin/viewAdmin',{account :req.session.user_accounttype} )
 	 	}
 	 })
 }
