c08ded8b5c0f23346a11af341fb6a272b4fbdf4f
SandeshOnRails <basnetsandesh450@gmail.com>
2018-08-08 11:51:13 -0700

    empty search added

--- models/test.js
+++ models/test.js
@@ -13,7 +13,7 @@ con.connect(function(err) {
 
   console.log(key.toLowerCase())
 
-  if(!(key.toLowerCase() === 'all')){
+  if(!(key.toLowerCase() === 'all' || key ==== '')){
 
  con.query("SELECT * FROM image WHERE photo_categorie LIKE '%" + key + "%'", function(err, result, fields) {
               
