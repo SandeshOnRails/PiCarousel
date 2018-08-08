2e01b16b2a0b817d39ea3deedce60049703f84c3
sirfeedsalot <sirfeedsalot@users.noreply.github.com>
2018-08-08 02:26:36 -0700

    Official app.js and upload.js

--- models/upload.js
+++ models/upload.js
@@ -4,46 +4,46 @@
 module.exports = (con, data, callback) => {
 
 
-con.connect(function(err) {
-
-  if(err) {
-  	console.log("error connecting: " + err)
-  }
-  else {
-  	console.log('connection successful')
-  }
-// query the database
-/*
-INSERT INTO  image
- (filepath, photo_categorie, description, owner_user_id, adminverified,licencetype,privacy,published,deleted,photo_regdate,title) 
- VALUES ('','','',3,0,0,0,0,0,now(),'');
-*/
-
-//con.query("INSERT INTO  image (filepath, photo_categorie, description, owner_user_id, adminverified,licencetype,privacy,published,deleted,title)   VALUES ('" + data.filepath + "','" + data.category + "','" + data.description + "'," + data.userID + ",0,0,0,0,0,'')", function(err, result, fields) {
-
- con.query("update image set filepath = '" + data.filename + "', photo_categorie ='" + data.category + "',title ='" + data.title + "', description ='" + data.description + "',  licencetype='" + data.licencetype +"',  privacy ='"+data.privacy +"'   where photo_id ="+data.photo_id , function(err, result, fields) {
-  //con.query("INSERT INTO  image (filepath, photo_categorie, title, description, owner_user_id, licencetype,privacy) VALUES ('
-// " + data.filename + "','" + data.category + "','" + data.title + "','" + data.description + "'," + data.userID + ",,'"+data.privacy +"')", function(err, result, fields) {
-
-              
-
-      if(err) {
-
-        console.log(err)
-
-      callback(false)
-      }
-
-        else { 
-             
-               console.log(result)
-              
-              callback(true)
-
+  con.connect(function(err) {
+  
+    if(err) {
+      console.log("error connecting: " + err)
+    }
+    else {
+      console.log('connection successful')
+    }
+  // query the database
+  /*
+  INSERT INTO  image
+   (filepath, photo_categorie, description, owner_user_id, adminverified,licencetype,privacy,published,deleted,photo_regdate,title) 
+   VALUES ('','','',3,0,0,0,0,0,now(),'');
+  */
+  
+  //con.query("INSERT INTO  image (filepath, photo_categorie, description, owner_user_id, adminverified,licencetype,privacy,published,deleted,title)   VALUES ('" + data.filepath + "','" + data.category + "','" + data.description + "'," + data.userID + ",0,0,0,0,0,'')", function(err, result, fields) {
+  
+   con.query("update image set filepath = '" + data.filename + "', photo_categorie ='" + data.category + "',title ='" + data.title + "', description ='" + data.description + "',  licencetype='" + data.licencetype +"',  privacy ='"+data.privacy +"'   where photo_id ="+data.photo_id , function(err, result, fields) {
+    //con.query("INSERT INTO  image (filepath, photo_categorie, title, description, owner_user_id, licencetype,privacy) VALUES ('
+  // " + data.filename + "','" + data.category + "','" + data.title + "','" + data.description + "'," + data.userID + ",,'"+data.privacy +"')", function(err, result, fields) {
+  
+                
+  
+        if(err) {
+  
+          console.log(err)
+  
+        callback(false)
         }
-});
-
-
-});
-    
-}
+  
+          else { 
+               
+                 console.log(result)
+                
+                callback(true)
+  
+          }
+  });
+  
+  
+  });
+      
+  }
